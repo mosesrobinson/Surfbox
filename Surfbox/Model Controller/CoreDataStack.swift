9d210d996cd89bea3801f2aad56f5a0444716f2c
Moses Robinson <mosesjrobinson@icloud.com>
2019-03-13 16:30:55 -0700

    changing model to fit new api

---
+++ Surfbox/Model Controller/CoreDataStack.swift
@@ -0,0 +1,48 @@
+//
+//  CoreDataStack.swift
+//  Surfbox
+//
+//  Created by Moses Robinson on 3/13/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import CoreData
+
+class CoreDataStack {
+    
+    static let shared = CoreDataStack()
+    
+    func save(context: NSManagedObjectContext = CoreDataStack.shared.mainContext) throws {
+        
+        var error: Error?
+        
+        context.performAndWait {
+            do {
+                try context.save()
+            } catch let saveError {
+                error = saveError
+            }
+        }
+        if let error = error { throw error }
+    }
+    
+    lazy var container: NSPersistentContainer = {
+        
+        let appName = Bundle.main.object(forInfoDictionaryKey: (kCFBundleNameKey as String)) as! String
+        let container = NSPersistentContainer(name: appName)
+        
+        container.loadPersistentStores { (_, error) in
+            
+            if let error = error {
+                fatalError("Failed to load persistent stores: \(error)")
+            }
+        }
+        
+        container.viewContext.automaticallyMergesChangesFromParent = true
+        return container
+    }()
+    
+    var mainContext: NSManagedObjectContext {
+        return container.viewContext
+    }
+}
---
+++ VODController.swift
@@ -0,0 +1,9 @@
+//
+//  VODController.swift
+//  Surfbox
+//
+//  Created by Moses Robinson on 3/13/19.
+//  Copyright © 2019 Moses Robinson. All rights reserved.
+//
+
+import Foundation
